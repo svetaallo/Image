0ef684643db386866d6eac01934d175a82369f2e
svetaallo <hellosedaya@gmail.com>
2022-07-18 20:22:26 +0500

    Добавьте файлы проекта.

---
+++ GrayscaleTask.cs
@@ -0,0 +1,32 @@
+﻿namespace Recognizer
+{
+	public static class GrayscaleTask
+	{
+		/* 
+		 * Переведите изображение в серую гамму.
+		 * 
+		 * original[x, y] - массив пикселей с координатами x, y. 
+		 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
+		 * 
+		 * Получившийся массив должен иметь те же размеры, 
+		 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
+		 *
+		 * Используйте формулу:
+		 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
+		 * 
+		 * Почему формула именно такая — читайте в википедии 
+		 * http://ru.wikipedia.org/wiki/Оттенки_серого
+		 */
+
+		public static double[,] ToGrayscale(Pixel[,] original)
+		{
+			var grayPic = new double[original.GetLength(0), original.GetLength(1)];
+			var height = original.GetLength(0);
+			var width = original.GetLength(1);
+			for (var x = 0; x < height; x++)
+				for (var y = 0; y < width; y++)
+					grayPic[x,y] = (original[x, y].R * 0.299 + original[x, y].G * 0.587 + original[x, y].B * 0.114) / 255;
+			return grayPic;
+		}
+	}
+}
---
+++ MedianFilterTask.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+namespace Recognizer
+{
+	internal static class MedianFilterTask
+	{
+		/* 
+		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
+		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
+		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
+		 * https://en.wikipedia.org/wiki/Median_filter
+		 * 
+		 * Используйте окно размером 3х3 для не граничных пикселей,
+		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
+		 */
+		public static double[,] MedianFilter(double[,] original)
+		{
+			return original;
+		}
+
+		public static double [,] AngleSorter(double[,]original)
+        {
+			var height = original.GetLength(0);
+			var width = original.GetLength(1);
+			var buf = new double[2, 2];
+			var i = 0;
+			var j = 0;
+            for (var x = 0; x < height; x += height - 1)
+            {
+				for (var y = 0; y < width; y += width - 1)
+                {
+					buf[i,j]=
+					j++;
+                }
+				i++;
+            }
+			return original;
+        }
+
+		public static double[,] EdgeSorter(double[,] original)
+		{
+
+			return original;
+		}
+		public static double[,] CentralSorter(double[,] original)
+		{
+
+			return original;
+		}
+	}
+}
